b194b23d3eee05a1f2b250abc0f6e617ce1eee99
AlFahadMunna <afmunna.afm.afm@gmail.com>
2026-03-08 11:40:12 -0500

    loader showing and hiding function added

---
+++ script.js
@@ -0,0 +1,9 @@
+let allIssues = [];
+
+const showLoader = () => {
+  document.getElementById("global-loader").classList.remove("hidden");
+};
+
+const hideLoader = () => {
+  document.getElementById("global-loader").classList.add("hidden");
+};
